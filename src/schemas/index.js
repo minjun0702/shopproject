be4688ced5c46b26d85a6e1a06e26f8008e6c537
leeminjun <mlnsuk0702@gmail.com>
2024-05-17 06:22:01 +0900

    오류체크

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.MONGODB_URL);
 const connect = () => {
   mongoose
     .connect(process.env.MONGODB_URL, {
       dbName: process.env.MONGODB_NAME,
     })
     .then(() => console.log("서버 연결에 성공하였습니다."))
-    .catch((err) => console.log(`서버 연결에 실패하였습니다. ${err}`));
+    .catch((err) =>
+      console.log(`서버 연결에 실패하였습니다. ${err}`),
+    );
 };
 
 mongoose.connection.on("error", (err) => {
